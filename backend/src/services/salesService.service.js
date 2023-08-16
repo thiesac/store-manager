7066b94d4f88df21042c5c78ef82b07aba29335a
Thiesa <thiesa.c@gmail.com>
2023-08-16 19:01:49 -0400

    Requisito 6 implementado e sem testes

--- backend/src/services/salesService.service.js
+++ backend/src/services/salesService.service.js
@@ -15,15 +15,15 @@ const findById = async (salesId) => {
   return { status: 200, data: sale };
 };
 
-const validateInsertSale = async (req, res, next) => {
-  const itemsSold = req.body;
-  for (const { productId } of itemsSold) {
+const validateInsertSale = async (itemsSold) => {
+  const promises = await Promise.all(itemsSold.map(async ({ productId }) => {
     const product = await productModel.findById(productId);
-    if (!product) {
-      return res.status(400).json({ message: '"productId" is required' });
+    if (product === undefined) {
+      return { status: 404, data: { message: 'Product not found' } };
     }
-
-  next();
+    return product;
+  }));
+  return promises;
 };
 
 const insertSale = async (itemsSold) => {
@@ -32,12 +32,17 @@ const insertSale = async (itemsSold) => {
     const errorMessages = validationResult.error.details.map((detail) => detail.message);
     return { status: 400, data: { message: errorMessages } };
   }
+  const validationId = await validateInsertSale(itemsSold);
+  console.log(validationId);
+  if (validationId[0].status === 404) {
+    return { status: 404, data: { message: 'Product not found' } };
+  }
   const sale = await salesModel.insertSale(itemsSold);
   return { status: 201, data: sale };
 };
 
-module.exports = {
-  findAll,
-  findById,
-  insertSale,
-};
+  module.exports = {
+    findAll,
+    findById,
+    insertSale,
+  };
